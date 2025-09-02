c4895d4ec8a09c1e99f4507fbe6c8b1b193a539b
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-09-02 18:28:09 +0500

    BST Insert value functionality in js

--- javascript/BST.js
+++ javascript/BST.js
@@ -33,6 +33,78 @@ function Tree(array = []) {
         return rootNode;
     })(array);
 
+    this.prettyPrint = function(node = this.rootNode, prefix = '', isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
+    this.insert = function(traverseNode = this.rootNode, value) {
+        if (traverseNode === null) {
+            this.rootNode.value = value;
+            return;
+        }
+
+        if (traverseNode.value !== null) {
+            if (value > this.rootNode.data) {
+                this.insert(traverseNode.right, value);
+            } else if (value < this.rootNode.data) {
+                this.insert(traverseNode.left, value);
+            }
+        } else {
+            traverseNode.value = value;
+            return;
+        }
+    }
+    this.deleteItem = function(traverseNode = this.rootNode, value) {
+        if (traverseNode === null) {
+            throw Error("Tree doesn't even exist!");
+        }
+
+        if (traverseNode.value !== null) {
+            if (value > this.rootNode.data) {
+                this.insert(traverseNode.right, value);
+            } else if (value < this.rootNode.data) {
+                this.insert(traverseNode.left, value);
+            }
+        } else {
+            throw Error("Value does not exist gang!");
+        }
+    }
+
+    this.find = function(value) {
+
+    }
+
+    this.levelOrderForEach = function(callback) { //  use an array acting as a queue to keep track of all the child nodes that you have yet to traverse and to add new ones to the list
+
+    }
+    this.inOrderForEach = function(callback) {
+
+    }
+    this.preOrderForEach = function(callback) {
+
+    }
+    this.postOrderForEach = function(callback) {
+
+    }
+    this.height = function(value) {
+
+    }
+    this.depth = function(value) {
+        
+    }
+    this.isBalanced = function() {
+        
+    }
 }
+
+const newTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 
